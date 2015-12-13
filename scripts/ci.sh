47c3d9cef66bf63500ca25890c91805e387412a6
bryanl <bryan@osesm.com>
2015-12-13 17:27:17 -0500

    ci script fixups

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-curl -L -o /tmp/glide-0.8.0-linux-amd64.tar.gz https://github.com/Masterminds/glide/releases/download/0.8.0/glide-0.8.0-linux-amd64.tar.gz
-tar -C /tmp -xf /tmp/glide-0.8.0-linux-amd64.tar.gz
+VER=0.8.0
+
+curl -L -o /tmp/glide-${VER}-linux-amd64.tar.gz https://github.com/Masterminds/glide/releases/download/${VER}/glide-${VER}-linux-amd64.tar.gz
+tar -C /tmp -xf /tmp/glide-${VER}-linux-amd64.tar.gz
 cp /tmp/linux-amd64/glide /usr/local/bin
 
-glide install
-go test $(glide nv)
+GLIDE=/tmp/linux-amd64/glide
+
+${GLIDE} install
+go test $(${GLIDE} nv)
 
