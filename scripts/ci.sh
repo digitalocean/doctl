1031c270e15923ca0400952c3a17fce85fa760b1
bryanl <bryan@osesm.com>
2015-12-13 17:28:30 -0500

    ci script fixups

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,7 +6,6 @@ VER=0.8.0
 
 curl -L -o /tmp/glide-${VER}-linux-amd64.tar.gz https://github.com/Masterminds/glide/releases/download/${VER}/glide-${VER}-linux-amd64.tar.gz
 tar -C /tmp -xf /tmp/glide-${VER}-linux-amd64.tar.gz
-cp /tmp/linux-amd64/glide /usr/local/bin
 
 GLIDE=/tmp/linux-amd64/glide
 
