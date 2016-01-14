a4bd8b967bc8c46125bf0a366c484c52abde1882
bryanl <bryan@osesm.com>
2016-01-14 15:13:54 -0500

    update glide to 0.8.3

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VER=0.8.0
+VER=0.8.3
 
 curl -L -o /tmp/glide-${VER}-linux-amd64.tar.gz https://github.com/Masterminds/glide/releases/download/${VER}/glide-${VER}-linux-amd64.tar.gz
 tar -C /tmp -xf /tmp/glide-${VER}-linux-amd64.tar.gz
