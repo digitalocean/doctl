0584055c48fa2dd8455ff041e56a0de0b57812bb
bryanl <bryan@osesm.com>
2016-03-06 20:02:30 -0500

    specify location of generated docs

--- cmd/doit-gen-doc/main.go
+++ cmd/doit-gen-doc/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
 	"github.com/bryanl/doit/commands"
 	"github.com/spf13/cobra/doc"
 )
 
+var (
+	outputDir = flag.String("outputDir", "./", "output directory")
+)
+
 func main() {
+	flag.Parse()
+
 	log.SetPrefix("doit: ")
 	cmd := commands.Init()
 	cmd.DisableAutoGenTag = false
 
-	doc.GenMarkdownTree(cmd, "./")
+	if _, err := os.Stat(*outputDir); os.IsNotExist(err) {
+		log.Fatalf("output directory %q does not exist", *outputDir)
+	}
+
+	doc.GenMarkdownTree(cmd, *outputDir)
 }
