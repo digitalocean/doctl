079fd4afcedece6d1aeff5b47bde79c5fce9b23a
bryanl <bryan@osesm.com>
2016-03-11 10:37:03 -0500

    generate docs

--- cmd/doit-gen-doc/main.go
+++ cmd/doit-gen-doc/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/bryanl/doit/commands"
 	"github.com/spf13/cobra/doc"
 )
+
+const fmTemplate = `---
+date: %s
+title: "%s"
+slug: %s
+url: %s
+---
+`
 
 var (
 	outputDir = flag.String("outputDir", "./", "output directory")
@@ -24,5 +37,18 @@ func main() {
 		log.Fatalf("output directory %q does not exist", *outputDir)
 	}
 
-	doc.GenMarkdownTree(cmd, *outputDir)
+	filePrepender := func(filename string) string {
+		now := time.Now().Format(time.RFC3339)
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, path.Ext(name))
+		url := "/commands/" + strings.ToLower(base) + "/"
+		return fmt.Sprintf(fmTemplate, now, strings.Replace(base, "_", " ", -1), base, url)
+	}
+
+	linkHandler := func(name string) string {
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return "/commands/" + strings.ToLower(base) + "/"
+	}
+
+	doc.GenMarkdownTreeCustom(cmd, *outputDir, filePrepender, linkHandler)
 }
