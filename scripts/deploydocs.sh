11a84f07093fdd6e2fa0f2f3176a5605f583a7f8
bryanl <bryan@osesm.com>
2016-03-16 21:18:36 -0400

    mkdir docs directory

--- scripts/deploydocs.sh
+++ scripts/deploydocs.sh
@@ -70,7 +70,7 @@ parse_args() {
 	# vars should be declared here, with sane defaults if applicable.
 
 	# Source directory & target branch.
-	deploy_directory=${GIT_DEPLOY_DIR:-dist}
+	deploy_directory=${GIT_DEPLOY_DIR:-"dist"}
 	deploy_branch=${GIT_DEPLOY_BRANCH:-gh-pages}
 
 	#if no user identity is already set in the current git environment, use this:
