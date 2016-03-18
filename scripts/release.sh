00d21938af0b2affe4318d8a516bfb2c9da851df
bryanl <bryan@osesm.com>
2016-03-18 11:22:06 -0400

    build release archives and sha256 sums

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env
 
 set -eo pipefail
 
@@ -9,27 +9,51 @@ if [[ -z "$ver" ]]; then
   exit 1
 fi
 
-
+RELEASE_PACKAGE=github.com/bryanl/doit/cmd/doctl
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR="${DIR}/../builds/${ver}"
+STAGE_DIR=$OUTPUT_DIR/stage
+RELEASE_DIR=$OUTPUT_DIR/release
+
+mkdir -p $OUTPUT_DIR/stage $OUTPUT_DIR/release
+
+rm -f $STAGE_DIR/doctl $STAGE_DIR/doctl.exe
 
 if [[ -z $SKIPBUILD ]]; then
-  mkdir -p $OUTPUT_DIR
-
   xgo \
-    --dest $OUTPUT_DIR \
+    --dest $OUTPUT_DIR/stage \
     --targets='windows/*,darwin/amd64,linux/amd64,linux/386' \
     -ldflags "-X github.com/bryanl/doit.Build=$(git rev-parse HEAD)" \
-    -out doit-${ver} github.com/bryanl/doit/cmd/doit
-
+    -out doit-${ver} $RELEASE_PACKAGE
 fi
 
-# FIXME mac only for now
-cd $OUTPUT_DIR
-echo $CWD
-for f in $(find . -maxdepth 1 -perm -111 -type f); do
-  fn=$(basename $f)
-  echo "generating sha256 checksum for $fn"
-  shasum -a 256 ${fn} > ${f}.sha256
-done
+cd $RELEASE_DIR
 
+for f in $STAGE_DIR/*; do
+  distfile=$(basename ${f%".exe"})
+  if [[ $f == *"windows"* ]]; then
+    distfile=${distfile}.zip
+  else
+    distfile=${distfile}.tar.gz
+  fi
+  
+  distbin=$(basename $RELEASE_PACKAGE)
+  if [[ $f == *.exe ]]; then
+    distbin=$distbin.exe
+  fi
+  
+  bindir=$STAGE_DIR/$distbin
+  cp $f $bindir
+  
+  if [[ $distfile == *.zip ]]; then
+    zip -j $distfile $bindir
+  else
+    tar cvzhf $distfile -C $STAGE_DIR $distbin
+  fi
+  
+  pushd $STAGE_DIR
+  shasum -a 256 $(basename $distbin) > ${RELEASE_DIR}/$(basename ${f%".exe"}).sha256
+  popd
+  
+  rm $bindir
+done
