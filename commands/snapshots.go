1fa7aab1cf16d1bf8b477c362dad0e2ff60bdbc9
Marko Mudrinic <mudrinic.mare@gmail.com>
2016-12-01 21:01:38 +0100

    added delete function (wip)

--- commands/snapshots.go
+++ commands/snapshots.go
@@ -1,6 +1,9 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/digitalocean/doctl"
+	"github.com/spf13/cobra"
+)
 
 func Snapshot() *Command {
 	cmd := &Command{
@@ -16,11 +19,17 @@ func Snapshot() *Command {
 	CmdBuilder(cmd, RunSnapshotList, "list", "list", Writer,
 		aliasOpt("ls"), displayerType(&snapshot{}), docCategories("droplet"))
 
-	CmdBuilder(cmd, RunSnapshotListVolume, "list volume", "list volume", Writer,
+	CmdBuilder(cmd, RunSnapshotListVolume, "lvolume", "list volume", Writer,
 		aliasOpt("lsv"), displayerType(&snapshot{}), docCategories("droplet"))
 
-	CmdBuilder(cmd, RunSnapshotListDroplet, "list droplet", "list droplet", Writer,
+	CmdBuilder(cmd, RunSnapshotListDroplet, "ldroplet", "list droplet", Writer,
 		aliasOpt("lsd"), displayerType(&snapshot{}), docCategories("droplet"))
+
+	/*	cmdRunDropletGet := CmdBuilder(cmd, RunSnapshotGet, "get", "get snapshot", Writer,
+		aliasOpt("g"), displayerType(&droplet{}), docCategories("droplet"))*/
+
+	CmdBuilder(cmd, RunSnapshotDelete, "delete", "delete snapshot", Writer,
+		aliasOpt("d"), displayerType(&droplet{}), docCategories("droplet"))
 
 	return cmd
 }
@@ -56,4 +65,46 @@ func RunSnapshotListDroplet(c *CmdConfig) error {
 	}
 	item := &snapshot{snapshots: list}
 	return c.Display(item)
+}
+
+/*func RunSnapshotGet(c *CmdConfig) error {
+	snapshotId, err := getSnapshotIDArg(c.NS, c.Args)
+	if err != nil {
+		return err
+	}
+
+	ss := c.Snapshots()
+
+	s, err := ss.Get(snapshotId)
+	if err != nil {
+		return err
+	}
+
+	//item := &snapshot{snapshots: do.Snapshot{*s}}
+
+	return nil
+}*/
+
+func RunSnapshotDelete(c *CmdConfig) error {
+	snapshotId, err := getSnapshotIDArg(c.NS, c.Args)
+	if err != nil {
+		return err
+	}
+
+	ss := c.Snapshots()
+
+	derr := ss.Delete(snapshotId)
+	if derr != nil {
+		return derr
+	}
+
+	return nil
+}
+
+func getSnapshotIDArg(ns string, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", doctl.NewMissingArgsErr(ns)
+	}
+
+	return args[0], nil
 }
