b6acca273f85c3a1233cb7676e31e68f49ce73c7
Hilary Holz <hholz@digitalocean.com>
2019-07-15 13:26:01 -0700

    clean up build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -eo pipefail
+set -eou pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUT_DIR="$DIR/../out"
-mkdir -p $OUT_DIR
+mkdir -p "$OUT_DIR"
 
 go build \
-  -o $OUT_DIR/doctl \
-  -ldflags "-X github.com/digitalocean/doctl/Build=`git rev-parse --short HEAD`" \
+  -o "$OUT_DIR/doctl" \
+  -ldflags "-X github.com/digitalocean/doctl/Build=$(git rev-parse --short HEAD)" \
   github.com/digitalocean/doctl/cmd/doctl
 
-chmod +x $OUT_DIR/doctl
+chmod +x "$OUT_DIR/doctl"
