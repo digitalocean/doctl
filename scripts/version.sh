58628fdd3e97f8feaf7cac5a1519f2f9bae92ee4
Hilary Holz <hholz@digitalocean.com>
2019-07-14 18:26:59 -0700

    extract version logic into a script
    and be comprehensive about it

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,8 +4,12 @@ set -eou pipefail
 
 version=`git fetch --tags &>/dev/null | git tag -l | sort --version-sort | tail -n1 | cut -c 2-`
 
-num_changes=`git status --porcelain | wc -l`
+branch=`git rev-parse --abbrev-ref HEAD`
+if [[ $branch != 'master' ]]; then
+  version=${version}-${branch}
+fi
 
+num_changes=`git status --porcelain | wc -l`
 if [[ $num_changes -ne 0 ]]; then
   commit=`git rev-parse --short HEAD`
   version=${version}-${commit}
