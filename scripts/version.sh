b19e7b64450abb75c73213c160b6d579d1a4c3e4
Hilary Holz <hholz@digitalocean.com>
2019-07-22 13:20:45 -0700

    improve version.sh
    allow for detached HEAD
    remove pipefail, as it causes false positives
    add '-s' option

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,52 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
+# do NOT use pipefail in this script, as it will cause problems with the version
+# line
+
+set -e
+
+me=$(basename "$0")
+
+help_message="\
+Usage: $me [<options>]
+Display doctl version
+
+Options:
+
+  -h, --help  Show this help information.
+  -s, --short major.minor.patch only
+"
+
+parse_args() {
+  while : ; do
+    if [[ $1 = "-h" || $1 = "--help" ]]; then
+      echo "$help_message"
+      return 0
+    elif [[ $1 = "-s" || $1 = "--short" ]]; then
+      short=true
+      shift
+    else
+      break
+    fi
+  done
+}
+
+parse_args "$@"
+
+if ! git diff --exit-code --quiet --cached; then
+  echo "Aborting due to uncommitted changes in the index" >&2
+  exit 1
+fi
 
 version=$(git fetch --tags &>/dev/null | git tag -l | sort --version-sort | tail -n1 | cut -c 2-)
 
+if [[ $short = true ]]; then
+  echo "$version"
+  exit 0
+fi
+
 branch=$(git rev-parse --abbrev-ref HEAD)
-if [[ $branch != 'master' ]]; then
+if [[ $branch != 'master' && $branch != HEAD ]]; then
   version=${version}-${branch}
 fi
 
