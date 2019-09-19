19dbf3fb04ba9c76054a1cc1470e42a6a18b4ef2
Zach Gershman <zgershman@digitalocean.com>
2019-09-19 10:06:00 -0700

    delete test script, windows uses make test now

--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-GO111MODULE="on" go test -race -mod=vendor ./commands/... ./do/... ./pkg/... .
-# disable until integration tests green on windows
-GO111MODULE="on" go test -race -mod=vendor ./integration
