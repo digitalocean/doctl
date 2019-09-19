09902ee1bd0f772a697d2b39cebc4d00bd0dd788
Zach Gershman <zgershman@digitalocean.com>
2019-09-19 09:42:01 -0700

    build a windows exe if we are on windows

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 GO111MODULE="on" go test -race -mod=vendor ./commands/... ./do/... ./pkg/... .
 # disable until integration tests green on windows
-#GO111MODULE="on" go test -race -mod=vendor ./integration
+GO111MODULE="on" go test -race -mod=vendor ./integration
