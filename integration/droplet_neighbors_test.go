e9fc51c75840228356a1c77ec8e107e39f39da16
Zach Gershman <zgershman@digitalocean.com>
2019-10-03 16:30:30 -0700

    sets env vars for token and url

--- integration/droplet_neighbors_test.go
+++ integration/droplet_neighbors_test.go
@@ -2,10 +2,13 @@ package integration
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -15,18 +18,27 @@ import (
 
 func testDropletNeighbors(t *testing.T, when spec.G, it spec.S) {
 	var (
-		expect *require.Assertions
-		server *httptest.Server
+		expect     *require.Assertions
+		server     *httptest.Server
+		configPath string
 	)
 
 	it.Before(func() {
 		expect = require.New(t)
 
+		dir, err := ioutil.TempDir("", "doct-integratio-tests")
+		expect.NoError(err)
+
+		configPath = filepath.Join(dir, "config.yaml")
+
+		err = ioutil.WriteFile(configPath, []byte(dropletNeighborsConfig), 0644)
+		expect.NoError(err)
+
 		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			switch req.URL.Path {
 			case "/v2/droplets/1111/neighbors":
 				auth := req.Header.Get("Authorization")
-				if auth != "Bearer some-magic-token" {
+				if auth != "Bearer some-extra-token" {
 					w.WriteHeader(http.StatusUnauthorized)
 					return
 				}
@@ -43,15 +55,23 @@ func testDropletNeighbors(t *testing.T, when spec.G, it spec.S) {
 		}))
 	})
 
+	it.After(func() {
+		err := os.RemoveAll(configPath)
+		expect.NoError(err)
+	})
+
 	when("all required flags are passed", func() {
 		it("lists droplet kernels", func() {
 			cmd := exec.Command(builtBinaryPath,
-				"-t", "some-magic-token",
-				"-u", server.URL,
 				"compute",
 				"droplet",
 				"neighbors",
 				"1111",
+			)
+
+			cmd.Env = append(os.Environ(),
+				"DIGITALOCEAN_ACCESS_TOKEN=some-extra-token",
+				fmt.Sprintf("DIGITALOCEAN_API_URL=%s", server.URL),
 			)
 
 			output, err := cmd.CombinedOutput()
@@ -63,13 +83,16 @@ func testDropletNeighbors(t *testing.T, when spec.G, it spec.S) {
 	when("asking for particular headers", func() {
 		it("only lists thoses headers", func() {
 			cmd := exec.Command(builtBinaryPath,
-				"-t", "some-magic-token",
-				"-u", server.URL,
 				"compute",
 				"droplet",
 				"neighbors",
 				"1111",
 				"--format", "ID,Memory,VCPUs,Disk,Region",
+			)
+
+			cmd.Env = append(os.Environ(),
+				fmt.Sprintf("DIGITALOCEAN_API_URL=%s", server.URL),
+				fmt.Sprintf("DIGITALOCEAN_CONFIG=%s", configPath),
 			)
 
 			output, err := cmd.CombinedOutput()
@@ -78,6 +101,11 @@ func testDropletNeighbors(t *testing.T, when spec.G, it spec.S) {
 		})
 	})
 }
+
+const dropletNeighborsConfig = `
+---
+access-token: some-extra-token
+`
 
 const dropletNeighborsOutput = `
 ID      Name    Public IPv4    Private IPv4    Public IPv6    Memory    VCPUs    Disk    Region       Image                          Status    Tags    Features    Volumes
