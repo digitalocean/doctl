1134de7ed22a2e563abddcb456325a0ea6ca2585
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2019-10-09 17:11:00 -0400

    Add snapshot test.

---
+++ integration/snapshot_get_test.go
@@ -0,0 +1,197 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/snapshot/get", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/snapshots/53344211":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+				w.Write([]byte(snapshotGetDropletResponse))
+			case "/v2/snapshots/0a343fac-eacf-11e9-b96b-0a58ac144633":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+				w.Write([]byte(snapshotGetVolumeResponse))
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+	})
+
+	when("passed a droplet ID", func() {
+		it("displays the droplet snapshot", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"get",
+				"53344211",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotGetDropletOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passed a volume ID", func() {
+		it("displays the volume snapshot", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"get",
+				"0a343fac-eacf-11e9-b96b-0a58ac144633",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotGetVolumeOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing a format", func() {
+		it("displays only those columns", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"get",
+				"--format", "ID,ResourceType",
+				"53344211",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotGetFormatOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing no-header", func() {
+		it("displays only values, no headers", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"get",
+				"--no-header",
+				"0a343fac-eacf-11e9-b96b-0a58ac144633",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotGetNoHeaderOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing a format and no header together", func() {
+		it("displays only the value with no header", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"get",
+				"--format", "ID",
+				"--no-header",
+				"53344211",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotGetFormatNoHeaderOutput), strings.TrimSpace(string(output)))
+		})
+	})
+})
+
+const snapshotGetDropletResponse = `{
+  "snapshot": {
+    "id": "53344211",
+    "name": "ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842",
+    "regions": [
+      "nyc1"
+    ],
+    "created_at": "2019-10-09T19:57:59Z",
+    "resource_id": "162347943",
+    "resource_type": "droplet",
+    "min_disk_size": 25,
+    "size_gigabytes": 1.01,
+    "tags": []
+  }
+}
+`
+
+const snapshotGetVolumeResponse = `{
+  "snapshot": {
+    "id": "0a343fac-eacf-11e9-b96b-0a58ac144633",
+    "name": "volume-nyc1-01-1570651053836",
+    "regions": [
+      "nyc1"
+    ],
+    "created_at": "2019-10-09T19:57:36Z",
+    "resource_id": "e2068b37-eace-11e9-85ad-0a58ac14430f",
+    "resource_type": "volume",
+    "min_disk_size": 100,
+    "size_gigabytes": 0,
+    "tags": []
+  }
+}
+`
+
+const snapshotGetVolumeOutput = `
+ID                                      Name                            Created at              Regions    Resource ID                             Resource Type    Min Disk Size    Size        Tags
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume-nyc1-01-1570651053836    2019-10-09T19:57:36Z    [nyc1]     e2068b37-eace-11e9-85ad-0a58ac14430f    volume           100              0.00 GiB
+`
+
+const snapshotGetDropletOutput = `
+ID          Name                                        Created at              Regions    Resource ID    Resource Type    Min Disk Size    Size        Tags
+53344211    ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842    2019-10-09T19:57:59Z    [nyc1]     162347943      droplet          25               1.01 GiB
+`
+
+const snapshotGetFormatOutput = `
+ID          Resource Type
+53344211    droplet
+`
+
+const snapshotGetNoHeaderOutput = `
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume-nyc1-01-1570651053836    2019-10-09T19:57:36Z    [nyc1]    e2068b37-eace-11e9-85ad-0a58ac14430f    volume    100    0.00 GiB
+`
+
+const snapshotGetFormatNoHeaderOutput = `
+53344211
+`
---
+++ integration/snapshot_list_test.go
@@ -0,0 +1,360 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/snapshot/list", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/snapshots":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				q := req.URL.Query()
+				resource := q.Get("resource_type")
+				if resource == "droplet" {
+					w.Write([]byte(snapshotListDropletResponse))
+					return
+				}
+
+				if resource == "volume" {
+					w.Write([]byte(snapshotListVolumeResponse))
+					return
+				}
+
+				w.Write([]byte(snapshotListResponse))
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+	})
+
+	when("all required flags are passed", func() {
+		it("lists snapshots", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing a format", func() {
+		it("displays only those columns", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+				"--format", "ID,ResourceType",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListFormatOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing no-header", func() {
+		it("displays only values, no headers", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+				"--no-header",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListNoHeaderOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing region", func() {
+		it("displays only snapshots in the region", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+				"--region",
+				"nyc1",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListRegionOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing droplet as resource type", func() {
+		it("displays only droplet snapshots", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+				"--resource",
+				"droplet",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListDropletOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing volume as resource type", func() {
+		it("displays only volume snapshots", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+				"--resource",
+				"volume",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListVolumeOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("passing region and resource type together", func() {
+		it("displays only droplet snapshots in the region", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"snapshot",
+				"list",
+				"--resource",
+				"droplet",
+				"--region",
+				"nyc1",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(snapshotListDropletRegionOutput), strings.TrimSpace(string(output)))
+		})
+	})
+})
+
+const snapshotListResponse = `{
+  "snapshots": [
+    {
+      "id": "0a343fac-eacf-11e9-b96b-0a58ac144633",
+      "name": "volume-nyc1-01-1570651053836",
+      "regions": [
+        "nyc1"
+      ],
+      "created_at": "2019-10-09T19:57:36Z",
+      "resource_id": "e2068b37-eace-11e9-85ad-0a58ac14430f",
+      "resource_type": "volume",
+      "min_disk_size": 100,
+      "size_gigabytes": 0,
+      "tags": []
+    },
+    {
+      "id": "0e0adfa4-eacf-11e9-9e75-0a58ac14c13b",
+      "name": "volume-lon1-01-1570651061232",
+      "regions": [
+        "lon1"
+      ],
+      "created_at": "2019-10-09T19:57:42Z",
+      "resource_id": "fcaf04e4-eace-11e9-a09f-0a58ac14c0f4",
+      "resource_type": "volume",
+      "min_disk_size": 100,
+      "size_gigabytes": 0,
+      "tags": []
+    },
+    {
+      "id": "53344211",
+      "name": "ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842",
+      "regions": [
+        "nyc1"
+      ],
+      "created_at": "2019-10-09T19:57:59Z",
+      "resource_id": "162347943",
+      "resource_type": "droplet",
+      "min_disk_size": 25,
+      "size_gigabytes": 1.01,
+      "tags": []
+    },
+    {
+      "id": "53344231",
+      "name": "ubuntu-s-1vcpu-1gb-lon1-01-1570651124450",
+      "regions": [
+        "lon1"
+      ],
+      "created_at": "2019-10-09T19:58:50Z",
+      "resource_id": "162348013",
+      "resource_type": "droplet",
+      "min_disk_size": 25,
+      "size_gigabytes": 1.01,
+      "tags": []
+    }
+  ],
+  "links": {},
+  "meta": {
+    "total": 4
+  }
+}
+`
+
+const snapshotListDropletResponse = `{
+  "snapshots": [
+    {
+      "id": "53344211",
+      "name": "ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842",
+      "regions": [
+        "nyc1"
+      ],
+      "created_at": "2019-10-09T19:57:59Z",
+      "resource_id": "162347943",
+      "resource_type": "droplet",
+      "min_disk_size": 25,
+      "size_gigabytes": 1.01,
+      "tags": []
+    },
+    {
+      "id": "53344231",
+      "name": "ubuntu-s-1vcpu-1gb-lon1-01-1570651124450",
+      "regions": [
+        "lon1"
+      ],
+      "created_at": "2019-10-09T19:58:50Z",
+      "resource_id": "162348013",
+      "resource_type": "droplet",
+      "min_disk_size": 25,
+      "size_gigabytes": 1.01,
+      "tags": []
+    }
+  ],
+  "links": {},
+  "meta": {
+    "total": 2
+  }
+}
+`
+
+const snapshotListVolumeResponse = `{
+  "snapshots": [
+    {
+      "id": "0a343fac-eacf-11e9-b96b-0a58ac144633",
+      "name": "volume-nyc1-01-1570651053836",
+      "regions": [
+        "nyc1"
+      ],
+      "created_at": "2019-10-09T19:57:36Z",
+      "resource_id": "e2068b37-eace-11e9-85ad-0a58ac14430f",
+      "resource_type": "volume",
+      "min_disk_size": 100,
+      "size_gigabytes": 0,
+      "tags": []
+    },
+    {
+      "id": "0e0adfa4-eacf-11e9-9e75-0a58ac14c13b",
+      "name": "volume-lon1-01-1570651061232",
+      "regions": [
+        "lon1"
+      ],
+      "created_at": "2019-10-09T19:57:42Z",
+      "resource_id": "fcaf04e4-eace-11e9-a09f-0a58ac14c0f4",
+      "resource_type": "volume",
+      "min_disk_size": 100,
+      "size_gigabytes": 0,
+      "tags": []
+    }
+  ],
+  "links": {},
+  "meta": {
+    "total": 2
+  }
+}
+`
+
+const snapshotListOutput = `
+ID                                      Name                                        Created at              Regions    Resource ID                             Resource Type    Min Disk Size    Size        Tags
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume-nyc1-01-1570651053836                2019-10-09T19:57:36Z    [nyc1]     e2068b37-eace-11e9-85ad-0a58ac14430f    volume           100              0.00 GiB    
+0e0adfa4-eacf-11e9-9e75-0a58ac14c13b    volume-lon1-01-1570651061232                2019-10-09T19:57:42Z    [lon1]     fcaf04e4-eace-11e9-a09f-0a58ac14c0f4    volume           100              0.00 GiB    
+53344211                                ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842    2019-10-09T19:57:59Z    [nyc1]     162347943                               droplet          25               1.01 GiB    
+53344231                                ubuntu-s-1vcpu-1gb-lon1-01-1570651124450    2019-10-09T19:58:50Z    [lon1]     162348013                               droplet          25               1.01 GiB
+`
+const snapshotListFormatOutput = `
+ID                                      Resource Type
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume
+0e0adfa4-eacf-11e9-9e75-0a58ac14c13b    volume
+53344211                                droplet
+53344231                                droplet
+`
+
+const snapshotListNoHeaderOutput = `
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume-nyc1-01-1570651053836                2019-10-09T19:57:36Z    [nyc1]    e2068b37-eace-11e9-85ad-0a58ac14430f    volume     100    0.00 GiB    
+0e0adfa4-eacf-11e9-9e75-0a58ac14c13b    volume-lon1-01-1570651061232                2019-10-09T19:57:42Z    [lon1]    fcaf04e4-eace-11e9-a09f-0a58ac14c0f4    volume     100    0.00 GiB    
+53344211                                ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842    2019-10-09T19:57:59Z    [nyc1]    162347943                               droplet    25     1.01 GiB    
+53344231                                ubuntu-s-1vcpu-1gb-lon1-01-1570651124450    2019-10-09T19:58:50Z    [lon1]    162348013                               droplet    25     1.01 GiB
+`
+
+const snapshotListRegionOutput = `
+ID                                      Name                                        Created at              Regions    Resource ID                             Resource Type    Min Disk Size    Size        Tags
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume-nyc1-01-1570651053836                2019-10-09T19:57:36Z    [nyc1]     e2068b37-eace-11e9-85ad-0a58ac14430f    volume           100              0.00 GiB    
+53344211                                ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842    2019-10-09T19:57:59Z    [nyc1]     162347943                               droplet          25               1.01 GiB
+`
+
+const snapshotListDropletOutput = `
+ID          Name                                        Created at              Regions    Resource ID    Resource Type    Min Disk Size    Size        Tags
+53344211    ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842    2019-10-09T19:57:59Z    [nyc1]     162347943      droplet          25               1.01 GiB    
+53344231    ubuntu-s-1vcpu-1gb-lon1-01-1570651124450    2019-10-09T19:58:50Z    [lon1]     162348013      droplet          25               1.01 GiB
+`
+
+const snapshotListVolumeOutput = `
+ID                                      Name                            Created at              Regions    Resource ID                             Resource Type    Min Disk Size    Size        Tags
+0a343fac-eacf-11e9-b96b-0a58ac144633    volume-nyc1-01-1570651053836    2019-10-09T19:57:36Z    [nyc1]     e2068b37-eace-11e9-85ad-0a58ac14430f    volume           100              0.00 GiB    
+0e0adfa4-eacf-11e9-9e75-0a58ac14c13b    volume-lon1-01-1570651061232    2019-10-09T19:57:42Z    [lon1]     fcaf04e4-eace-11e9-a09f-0a58ac14c0f4    volume           100              0.00 GiB
+`
+
+const snapshotListDropletRegionOutput = `
+ID          Name                                        Created at              Regions    Resource ID    Resource Type    Min Disk Size    Size        Tags
+53344211    ubuntu-s-1vcpu-1gb-nyc1-01-1570651077842    2019-10-09T19:57:59Z    [nyc1]     162347943      droplet          25               1.01 GiB
+`
