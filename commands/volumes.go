4f15ce45752291b7d06e2ffabc94e87cc70c84d3
mgibson323 <m.gibson323@gmail.com>
2019-10-08 17:13:12 -0700

    Add extended documentation for volume command

--- commands/volumes.go
+++ commands/volumes.go
@@ -30,34 +30,40 @@ func Volume() *Command {
 	cmd := &Command{
 		Command: &cobra.Command{
 			Use:   "volume",
-			Short: "volume commands",
-			Long:  "volume is used to access volume commands",
+			Short: "Provides commands that manage Block Storage volumes",
+			Long: `The sub-commands of 'doctl compute volume' manage your Block Storage volumes.
+
+Block Storage volumes provide expanded storage capacity for your Droplets and can be moved between Droplets within a specific region.
+Volumes function as raw block devices, meaning they appear to the operating system as locally attached storage which can be formatted using any filesystem supported by the OS. They may be created in sizes from 1GiB to 16TiB.`,
 		},
 	}
 
-	cmdRunVolumeList := CmdBuilder(cmd, RunVolumeList, "list", "list volume", Writer,
+	cmdRunVolumeList := CmdBuilderWithDocs(cmd, RunVolumeList, "list", "Lists Block Storage volumes by ID", `Use this command to list all of the Block Storage volumes on your account.`, Writer,
 		aliasOpt("ls"), displayerType(&displayers.Volume{}))
 	AddStringFlag(cmdRunVolumeList, doctl.ArgRegionSlug, "", "", "Volume region")
 
-	cmdVolumeCreate := CmdBuilder(cmd, RunVolumeCreate, "create <volume-name>", "create a volume", Writer,
+	cmdVolumeCreate := CmdBuilderWithDocs(cmd, RunVolumeCreate, "create <volume-name>", "Create a Block Storage volume", `Use this command to create a Block Storage volume on your account. 
+You can use flags to specify the volume size, region, description, filesystem type, tags, and to create a volume from an existing volume snapshot.`, Writer,
 		aliasOpt("c"), displayerType(&displayers.Volume{}))
 	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeSize, "", "4TiB", "Volume size",
 		requiredOpt())
 	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeDesc, "", "", "Volume description")
 	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeRegion, "", "", "Volume region; should not be specified with a snapshot")
-	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeSnapshot, "", "", "Volume snapshot; should not specified with a region")
+	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeSnapshot, "", "", "Volume snapshot; should not be specified with a region")
 	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeFilesystemType, "", "", "Volume filesystem type (ext4 or xfs)")
 	AddStringFlag(cmdVolumeCreate, doctl.ArgVolumeFilesystemLabel, "", "", "Volume filesystem label")
 	AddStringSliceFlag(cmdVolumeCreate, doctl.ArgTag, "", []string{}, "tags to apply to the volume; comma separate or repeat --tag to add multiple tags at once")
 
-	cmdRunVolumeDelete := CmdBuilder(cmd, RunVolumeDelete, "delete <volume-id>", "delete a volume", Writer,
+	cmdRunVolumeDelete := CmdBuilderWithDocs(cmd, RunVolumeDelete, "delete <volume-id>", "Delete a Block Storage volume", `Use this command to delete a Block Storage volume by ID, destroying all of its data and removing it from your account.`, Writer,
 		aliasOpt("rm", "d"))
 	AddBoolFlag(cmdRunVolumeDelete, doctl.ArgForce, doctl.ArgShortForce, false, "Force volume delete")
 
-	CmdBuilder(cmd, RunVolumeGet, "get <volume-id>", "get a volume", Writer, aliasOpt("g"),
+	CmdBuilderWithDocs(cmd, RunVolumeGet, "get <volume-id>", "Retrieve an existing Block Storage volume", `Use this command to show information about a Block Storage volume by ID`, Writer, aliasOpt("g"),
 		displayerType(&displayers.Volume{}))
 
-	cmdRunVolumeSnapshot := CmdBuilder(cmd, RunVolumeSnapshot, "snapshot <volume-id>", "create a volume snapshot", Writer,
+	cmdRunVolumeSnapshot := CmdBuilderWithDocs(cmd, RunVolumeSnapshot, "snapshot <volume-id>", "Create a Block Storage volume snapshot", `Use this command to create a snapshot of a Block Storage volume by ID. 
+
+You can use a Block Storage volume snapshot ID as a flag with "doctl volume create" to create a new Block Storage volume with the same data as the volume the snapshot was taken from.`, Writer,
 		aliasOpt("s"), displayerType(&displayers.Volume{}))
 	AddStringFlag(cmdRunVolumeSnapshot, doctl.ArgSnapshotName, "", "", "Snapshot name", requiredOpt())
 	AddStringFlag(cmdRunVolumeSnapshot, doctl.ArgSnapshotDesc, "", "", "Snapshot description")
