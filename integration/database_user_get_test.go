c473bbfcad52202abaa89afa21319130a524d2c9
Zach Gershman <zgershman@digitalocean.com>
2019-10-11 10:04:37 -0700

    adds database user tests

---
+++ integration/database_user_get_test.go
@@ -0,0 +1,126 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("database/user/get", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/databases/some-database-id/users/some-user-id":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusTeapot)
+				}
+
+				if req.Method != "GET" {
+					w.WriteHeader(http.StatusTeapot)
+					return
+				}
+
+				w.Write([]byte(databaseUserGetResponse))
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+	})
+
+	when("all required flags are passed", func() {
+		it("gets the database user", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"database",
+				"user",
+				"get",
+				"some-database-id",
+				"some-user-id",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(databaseUserGetOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("no header is passed", func() {
+		it("does not display the header", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"database",
+				"user",
+				"get",
+				"some-database-id",
+				"some-user-id",
+				"--no-header",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(databaseUserGetNoHeaderOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("format is passed", func() {
+		it("presents the columns requested", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"database",
+				"user",
+				"get",
+				"some-database-id",
+				"some-user-id",
+				"--format", "Name",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(databaseUserGetFormatOutput), strings.TrimSpace(string(output)))
+		})
+	})
+})
+
+const databaseUserGetOutput = `
+Name            Role      Password
+some-user-id    normal    jge5lfxtzhx42iff
+`
+const databaseUserGetNoHeaderOutput = `
+some-user-id    normal    jge5lfxtzhx42iff
+`
+const databaseUserGetFormatOutput = `
+Name
+some-user-id
+`
+const databaseUserGetResponse = `
+{
+  "user": {
+    "name": "some-user-id",
+    "role": "normal",
+    "password": "jge5lfxtzhx42iff"
+  }
+}
+`
