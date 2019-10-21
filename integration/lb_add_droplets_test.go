488fbcaaa9b0e2f3c8a6cfac3592a776fa83c3d5
Zach Gershman <zgershman@digitalocean.com>
2019-10-21 14:32:32 -0700

    adds lb add droplets test

---
+++ integration/lb_add_droplets_test.go
@@ -0,0 +1,72 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/load-balancer/add-droplets", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/load_balancers/my-droplet-id/droplets":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "POST" {
+					w.WriteHeader(http.StatusTeapot)
+					return
+				}
+
+				reqBody, err := ioutil.ReadAll(req.Body)
+				expect.NoError(err)
+
+				expect.JSONEq(`{"droplet_ids": [111,222,444]}`, string(reqBody))
+
+				w.WriteHeader(http.StatusNoContent)
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+	})
+
+	when("all required flags are passed", func() {
+		it("attaches droplets to a load balancer", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"load-balancer",
+				"add-droplets",
+				"my-droplet-id",
+				"--droplet-ids", "111,222,444",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+		})
+	})
+})
