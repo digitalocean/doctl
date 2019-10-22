646869044c33f0bda0f4c93a7713fd831075ea79
Zach Gershman <zgershman@digitalocean.com>
2019-10-22 12:08:00 -0700

    adds load balancer remove forwarding rules test

---
+++ integration/lb_remove_forwarding_rules.go
@@ -0,0 +1,84 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/load-balancer/remove-forwarding-rules", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/load_balancers/basic-load-id/forwarding_rules":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "DELETE" {
+					w.WriteHeader(http.StatusTeapot)
+					return
+				}
+
+				reqBody, err := ioutil.ReadAll(req.Body)
+				expect.NoError(err)
+
+				expect.JSONEq(lbRemoveForwardingRulesRequest, string(reqBody))
+
+				w.WriteHeader(http.StatusNoContent)
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+	})
+
+	when("all required flags are passed", func() {
+		it("removes the specified forwarding rules from the load balancer", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"load-balancer",
+				"remove-forwarding-rules",
+				"basic-load-id",
+				"--forwarding-rules", "entry_protocol:tcp,entry_port:3306,target_protocol:https,target_port:8443",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Empty(output)
+		})
+	})
+})
+
+const lbRemoveForwardingRulesRequest = `{
+"forwarding_rules":[
+    {
+      "entry_protocol":"tcp",
+      "entry_port":3306,
+      "target_protocol":"https",
+      "target_port":8443
+    }
+  ]
+}`
