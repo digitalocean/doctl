a64a010c850157572fe15f547fdd5853a186db53
Zach Gershman <zgershman@digitalocean.com>
2019-10-23 09:38:59 -0700

    adds volume list test

---
+++ integration/volume_list_test.go
@@ -0,0 +1,136 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/volume/list", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		cmd    *exec.Cmd
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/volumes":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "GET" {
+					w.WriteHeader(http.StatusTeapot)
+					return
+				}
+
+				w.Write([]byte(volumeListResponse))
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+
+		cmd = exec.Command(builtBinaryPath,
+			"-t", "some-magic-token",
+			"-u", server.URL,
+			"compute",
+			"volume",
+		)
+
+	})
+
+	when("command is list", func() {
+		it("lists all volumes", func() {
+			cmd.Args = append(cmd.Args, []string{"list"}...)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(volumeListOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+	when("command is ls", func() {
+		it("lists all volumes", func() {
+			cmd.Args = append(cmd.Args, []string{"ls"}...)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(volumeListOutput), strings.TrimSpace(string(output)))
+		})
+	})
+})
+
+const volumeListOutput = `
+ID                  Name       Size      Region    Filesystem Type    Filesystem Label    Droplet IDs    Tags
+some-volume-id-1    example    10 GiB    nyc1                                             [1]            aninterestingtag
+some-volume-id-2    example    40 GiB    nyc1                                             [2]            adifferenttag
+`
+const volumeListResponse = `
+{
+  "volumes": [
+    {
+      "id": "some-volume-id-1",
+      "region": {
+        "name": "New York 1",
+        "slug": "nyc1",
+        "sizes": ["s-1vcpu-1gb"],
+        "features": [
+          "private_networking",
+          "backups",
+          "ipv6",
+          "metadata"
+        ],
+        "available": true
+      },
+      "droplet_ids": [1],
+      "name": "example",
+      "description": "Block store for examples",
+      "size_gigabytes": 10,
+      "created_at": "2016-03-02T17:00:49Z",
+      "tags": ["aninterestingtag"]
+    },
+    {
+      "id": "some-volume-id-2",
+      "region": {
+        "name": "New York 1",
+        "slug": "nyc1",
+        "sizes": ["s-1vcpu-1gb"],
+        "features": [
+          "private_networking",
+          "backups",
+          "ipv6",
+          "metadata"
+        ],
+        "available": true
+      },
+      "droplet_ids": [2],
+      "name": "example",
+      "description": "Block store for examples",
+      "size_gigabytes": 40,
+      "created_at": "2016-03-02T17:10:49Z",
+      "tags": ["adifferenttag"]
+    }
+  ],
+  "links": {},
+  "meta": {
+    "total": 2
+  }
+}
+`
