17ad0f5471d32ca766d3a3f74375ef3519e24cd7
Zach Gershman <zgershman@digitalocean.com>
2019-10-23 09:18:40 -0700

    adds volume delete test

---
+++ integration/volume_delete_test.go
@@ -0,0 +1,84 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/volume/delete", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect   *require.Assertions
+		cmd      *exec.Cmd
+		baseArgs []string
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/volumes/my-volume-id":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "DELETE" {
+					w.WriteHeader(http.StatusTeapot)
+					return
+				}
+
+				w.WriteHeader(http.StatusNoContent)
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+
+		cmd = exec.Command(builtBinaryPath,
+			"-t", "some-magic-token",
+			"-u", server.URL,
+			"compute",
+			"volume")
+
+		baseArgs = []string{
+			"my-volume-id",
+			"--force",
+		}
+
+	})
+
+	when("command is delete", func() {
+		it("deletes the volume", func() {
+			args := append([]string{"delete"}, baseArgs...)
+			cmd.Args = append(cmd.Args, args...)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Empty(output)
+		})
+	})
+
+	when("command is rm", func() {
+		it("deletes the volume", func() {
+			args := append([]string{"rm"}, baseArgs...)
+			cmd.Args = append(cmd.Args, args...)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Empty(output)
+		})
+	})
+})
