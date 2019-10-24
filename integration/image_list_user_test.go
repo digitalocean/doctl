a2936b70180745d094b6ce1d0fb274c2564bd6da
Zach Gershman <zgershman@digitalocean.com>
2019-10-24 14:30:43 -0700

    adds last two list tests

---
+++ integration/image_list_user_test.go
@@ -0,0 +1,78 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/image/list-user", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/images":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "GET" {
+					w.WriteHeader(http.StatusMethodNotAllowed)
+					return
+				}
+
+				containsPrivate := req.URL.Query().Get("private")
+
+				if containsPrivate != "true" {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+
+				// Test uses the same return from ListApplication
+				// as the JSON returned is identical and it appears
+				// most of the filtering logic happens via the API
+				w.Write([]byte(imageListApplicationResponse))
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+
+	})
+
+	when("passing no flags", func() {
+		it("lists all user images", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"image",
+				"list-user",
+				"--public",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(imageListApplicationOutput), strings.TrimSpace(string(output)))
+		})
+	})
+})
