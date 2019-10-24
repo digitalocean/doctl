67dff7b267b3c7c7877f02adaaf9559428661ca9
Zach Gershman <zgershman@digitalocean.com>
2019-10-24 13:58:52 -0700

    adds image update test

---
+++ integration/image_update_test.go
@@ -0,0 +1,86 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/image/update", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect *require.Assertions
+		server *httptest.Server
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/images/7777":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "PUT" {
+					w.WriteHeader(http.StatusMethodNotAllowed)
+					return
+				}
+
+				reqBody, err := ioutil.ReadAll(req.Body)
+				expect.NoError(err)
+
+				expect.JSONEq(imageUpdateRequest, string(reqBody))
+
+				w.Write([]byte(imageUpdateResponse))
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+
+	})
+
+	when("when all flags are passed", func() {
+		it("updates the particular image name", func() {
+			cmd := exec.Command(builtBinaryPath,
+				"-t", "some-magic-token",
+				"-u", server.URL,
+				"compute",
+				"image",
+				"update",
+				"7777",
+				"--image-name", "some-new-name",
+			)
+
+			output, err := cmd.CombinedOutput()
+			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+			expect.Equal(strings.TrimSpace(imageUpdateOutput), strings.TrimSpace(string(output)))
+		})
+	})
+
+})
+
+const (
+	//Both imageUpdateOutput and imageUpdateResponse are
+	//exactly the same for get. We don't need additional
+	//return JSON as the parsing is functionally
+	//equivalent.
+	imageUpdateOutput   = imageGetOutput
+	imageUpdateRequest  = `{"name": "some-new-name"}`
+	imageUpdateResponse = imageGetResponse
+)
