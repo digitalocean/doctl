3d1be0f087d5a88b16d462affc8620ef7cf9c908
Zach Gershman <zgershman@digitalocean.com>
2019-10-28 11:06:58 -0700

    Revert "consolidate live and test flag semantics"
    This reverts commit 21183e6f5f06a1206ef2ce9dfb1bd49e04b5c805.

--- commands/kubernetes.go
+++ commands/kubernetes.go
@@ -1127,7 +1127,7 @@ func buildClusterCreateRequestFromArgs(c *CmdConfig, r *godo.KubernetesClusterCr
 	}
 
 	// multiple node pools
-	if c.Doit.IsSet(c.NS, doctl.ArgSizeSlug) || c.Doit.IsSet(c.NS, doctl.ArgNodePoolCount) {
+	if c.Doit.IsSet(doctl.ArgSizeSlug) || c.Doit.IsSet(doctl.ArgNodePoolCount) {
 		return fmt.Errorf("flags %q and %q cannot be provided when %q is present", doctl.ArgSizeSlug, doctl.ArgNodePoolCount, doctl.ArgClusterNodePool)
 	}
 
