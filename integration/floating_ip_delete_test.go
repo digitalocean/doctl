36ce8643961a95b1d2e3fe11ed9a13ac98f784b0
Zach Gershman <zgershman@digitalocean.com>
2019-10-29 09:42:35 -0700

    adds floating-ip delete test

---
+++ integration/floating_ip_delete_test.go
@@ -0,0 +1,82 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os/exec"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+)
+
+var _ = suite("compute/floating-ip/delete", func(t *testing.T, when spec.G, it spec.S) {
+	var (
+		expect   *require.Assertions
+		cmd      *exec.Cmd
+		baseArgs = []string{"1.1.1.1", "-f"}
+	)
+
+	it.Before(func() {
+		expect = require.New(t)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			switch req.URL.Path {
+			case "/v2/floating_ips/1.1.1.1":
+				auth := req.Header.Get("Authorization")
+				if auth != "Bearer some-magic-token" {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				if req.Method != "DELETE" {
+					w.WriteHeader(http.StatusMethodNotAllowed)
+					return
+				}
+
+				w.WriteHeader(http.StatusNoContent)
+			default:
+				dump, err := httputil.DumpRequest(req, true)
+				if err != nil {
+					t.Fatal("failed to dump request")
+				}
+
+				t.Fatalf("received unknown request: %s", dump)
+			}
+		}))
+
+		cmd = exec.Command(builtBinaryPath,
+			"-t", "some-magic-token",
+			"-u", server.URL,
+			"compute",
+			"floating-ip",
+		)
+	})
+
+	when("required flags are passed", func() {
+		cases := []struct {
+			desc    string
+			command string
+		}{
+			{desc: "command is delete", command: "delete"},
+			{desc: "command is d", command: "d"},
+		}
+
+		for _, c := range cases {
+			when(c.desc, func() {
+				command := c.command
+
+				it("deletes the specified floating-ip", func() {
+					args := append([]string{command}, baseArgs...)
+					cmd.Args = append(cmd.Args, args...)
+
+					output, err := cmd.CombinedOutput()
+					expect.NoError(err, fmt.Sprintf("received error output: %s", output))
+					expect.Empty(output)
+				})
+			})
+		}
+	})
+})
