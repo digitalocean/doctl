61afe4440c3b9d4fe6fb4fa95d8f867b24f7182f
Jordan Neufeld <jordan@neufeldtech.com>
2019-11-07 15:31:44 -0600

    Updated looksLikeUUID to support UUIDv1 and UUIDv4

--- commands/kubernetes_test.go
+++ commands/kubernetes_test.go
@@ -1001,24 +1001,33 @@ func TestLatestVersionForUpgrade(t *testing.T) {
 func Test_looksLikeUUID(t *testing.T) {
 	tests := []struct {
 		name string
+		arg  string
 		want bool
 	}{
 		{
-			name: "21464f77-42fc-4a32-9aa4-a101843e94c0",
+			name: "UUIDv1",
+			arg:  "a235f190-01a4-11ea-af17-4989e8155574",
+			want: true,
+		},
+		{
+			name: "UUIDv4",
+			arg:  "21464f77-42fc-4a32-9aa4-a101843e94c0",
 			want: true,
 		},
 		{
 			name: "my-cluster",
+			arg:  "my-cluster",
 			want: false,
 		},
 		{
 			name: "f8291060b73f4fa7b60586fe51a1d862",
+			arg:  "f8291060b73f4fa7b60586fe51a1d862",
 			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := looksLikeUUID(tt.name); got != tt.want {
+			if got := looksLikeUUID(tt.arg); got != tt.want {
 				t.Errorf("looksLikeUUID() = %v, want %v", got, tt.want)
 			}
 		})
