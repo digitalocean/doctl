e98680eb01a7c3668526ed3b6ce605c30d6d9ed2
Mike <mholly@digitalocean.com>
2019-11-20 15:22:03 -0800

    don't include auth mode in user output

--- integration/database_user_create_test.go
+++ integration/database_user_create_test.go
@@ -101,6 +101,7 @@ var _ = suite("database/user/create", func(t *testing.T, when spec.G, it spec.S)
 			)
 
 			output, err := cmd.CombinedOutput()
+			t.Log(string(output))
 			expect.NoError(err, fmt.Sprintf("received error output: %s", output))
 			expect.Equal(strings.TrimSpace(databaseUserCreateOutput), strings.TrimSpace(string(output)))
 		})
@@ -117,7 +118,8 @@ some-user-name    normal    jge5lfxtzhx42iff
   "user": {
     "name": "{{.Name}}",
     "role": "normal",
-    "password": "jge5lfxtzhx42iff"
+    "password": "jge5lfxtzhx42iff",
+    "mysql_settings": { "auth_plugin": "mysql_native_password" }
   }
 }
 `
