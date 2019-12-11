2c78636bc2004c2e84056b664ec0f22c62a7d5c1
Timo Reimann <treimann@digitalocean.com>
2019-12-11 15:41:58 +0100

    Fix link to block storage API reference (#287)
    The previous link does lead to the right anchor.

--- storage.go
+++ storage.go
@@ -15,7 +15,7 @@ const (
 
 // StorageService is an interface for interfacing with the storage
 // endpoints of the Digital Ocean API.
-// See: https://developers.digitalocean.com/documentation/v2#storage
+// See: https://developers.digitalocean.com/documentation/v2/#block-storage
 type StorageService interface {
 	ListVolumes(context.Context, *ListVolumeParams) ([]Volume, *Response, error)
 	GetVolume(context.Context, string) (*Volume, *Response, error)
