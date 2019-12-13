bf6f5badf7526be530d962c5c30674a43eab1e7e
Zach Gershman <zgershman@digitalocean.com>
2019-12-13 11:37:45 -0800

    prepares release of 1.29.0

--- godo.go
+++ godo.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.28.0"
+	libraryVersion = "1.29.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
