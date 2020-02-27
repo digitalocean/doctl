b1cd87eaa4fcdb966afc7882d0e09cd58bbcff4d
Ryan Butler <1170939+rbutler@users.noreply.github.com>
2020-02-27 15:47:52 -0600

    Add BillingHistory service and List endpoint

--- godo.go
+++ godo.go
@@ -46,6 +46,7 @@ type Client struct {
 	Account           AccountService
 	Actions           ActionsService
 	Balance           BalanceService
+	BillingHistory    BillingHistoryService
 	CDNs              CDNService
 	Domains           DomainsService
 	Droplets          DropletsService
@@ -168,6 +169,7 @@ func NewClient(httpClient *http.Client) *Client {
 	c.Account = &AccountServiceOp{client: c}
 	c.Actions = &ActionsServiceOp{client: c}
 	c.Balance = &BalanceServiceOp{client: c}
+	c.BillingHistory = &BillingHistoryServiceOp{client: c}
 	c.CDNs = &CDNServiceOp{client: c}
 	c.Certificates = &CertificatesServiceOp{client: c}
 	c.Domains = &DomainsServiceOp{client: c}
