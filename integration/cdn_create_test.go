9ec625a2b46acd76008363a3549e6f9beef28059
Nadya Djojosantoso <hello@nadyafebi.com>
2020-10-05 14:46:41 -0700

    Correct misspellings in the codebase

--- integration/cdn_create_test.go
+++ integration/cdn_create_test.go
@@ -81,7 +81,7 @@ var _ = suite("compute/cdn/create", func(t *testing.T, when spec.G, it spec.S) {
 				"compute",
 				"cdn",
 				"create",
-				"magic-orgin",
+				"magic-origin",
 				"--certificate-id", "some-cert-id",
 				"--domain", "example.com",
 				"--ttl", "60",
@@ -97,7 +97,7 @@ var _ = suite("compute/cdn/create", func(t *testing.T, when spec.G, it spec.S) {
 const (
 	cdnCreateOutput = `
 ID                                      Origin         Endpoint                                         TTL    CustomDomain    CertificateID    CreatedAt
-19f06b6a-3ace-4315-b086-499a0e521b76    magic-orgin    static-images.nyc3.cdn.digitaloceanspaces.com    60     example.com     some-cert-id     2018-07-19 15:04:16 +0000 UTC
+19f06b6a-3ace-4315-b086-499a0e521b76    magic-origin    static-images.nyc3.cdn.digitaloceanspaces.com    60     example.com     some-cert-id     2018-07-19 15:04:16 +0000 UTC
 `
 	cdnCreateResponse = `
 {
