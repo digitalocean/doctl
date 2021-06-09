75694540ddf985195c5fa058714f6adb5038050c
Sergej Roswall Bogachov <s.schade.s@gmail.com>
2021-06-09 18:35:52 +0200

    Allow node-pool args to be empty (#990)
    * Allow node-pool args to be empty
    
    * Trimming ; from the end of the nodePool arg
    
    * disallow empty args

--- commands/kubernetes.go
+++ commands/kubernetes.go
@@ -1720,7 +1720,8 @@ func parseNodePoolString(nodePool, defaultName, defaultSize string, defaultCount
 		Labels: map[string]string{},
 		Taints: []godo.Taint{},
 	}
-	for _, arg := range strings.Split(nodePool, argSeparator) {
+	trimmedPool := strings.TrimSuffix(nodePool, argSeparator)
+	for _, arg := range strings.Split(trimmedPool, argSeparator) {
 		kvs := strings.SplitN(arg, kvSeparator, 2)
 		if len(kvs) < 2 {
 			return nil, fmt.Errorf("A node pool string argument must be of the form `key=value`. Provided KVs: %v", kvs)
