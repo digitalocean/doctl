9c8d7168307906a0900938abaa4bd54346fbff26
Timo Reimann <treimann@digitalocean.com>
2022-11-16 15:55:31 +0100

    kubernetes: unhide high availability flag

--- commands/kubernetes.go
+++ commands/kubernetes.go
@@ -322,7 +322,6 @@ This command updates the specified configuration values for the specified Kubern
 		"Boolean specifying whether to enable surge-upgrade for the cluster")
 	AddBoolFlag(cmdKubeClusterUpdate, doctl.ArgHA, "", false,
 		"Boolean specifying whether to enable the highly-available control plane for the cluster")
-	cmdKubeClusterUpdate.Flags().MarkHidden(doctl.ArgHA) // disabled until HA enablement launch
 	AddBoolFlag(cmdKubeClusterUpdate, doctl.ArgClusterUpdateKubeconfig, "",
 		true, "Boolean specifying whether to update the cluster in your kubeconfig")
 	AddBoolFlag(cmdKubeClusterUpdate, doctl.ArgSetCurrentContext, "", true,
