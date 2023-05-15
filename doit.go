ba7fb33193d713ff07a039adf8b4929038b38274
Ben Tranter <ben@bentranter.io>
2023-05-15 17:26:37 -0400

    doit: respect json output flag (#1376)
    * doit: respect json output flag
    
    * doit: tweak version json output
    
    * doit: add tests for version json output

--- doit.go
+++ doit.go
@@ -139,6 +139,34 @@ func (v Version) Complete(lv LatestVersioner) string {
 	return buffer.String()
 }
 
+// CompleteJSON is the complete version for doit, formatted as JSON.
+func (v Version) CompleteJSON(lv LatestVersioner) string {
+	versionInfo := &struct {
+		Version      string `json:"version,omitempty"`
+		Commit       string `json:"commit,omitempty"`
+		Notification string `json:"notification,omitempty"`
+	}{
+		Version: v.String(),
+		Commit:  v.Build,
+	}
+
+	if tagName, err := lv.LatestVersion(); err == nil {
+		v0, err1 := semver.Make(tagName)
+		v1, err2 := semver.Make(v.String())
+
+		if len(v0.Build) == 0 {
+			v0, err1 = semver.Make(tagName + "-release")
+		}
+
+		if err1 == nil && err2 == nil && v0.GT(v1) {
+			versionInfo.Notification = fmt.Sprintf("release %s is available, check it out!", tagName)
+		}
+	}
+
+	data, _ := json.MarshalIndent(versionInfo, "", "  ")
+	return string(data)
+}
+
 // LatestVersioner an interface for detecting the latest version.
 type LatestVersioner interface {
 	LatestVersion() (string, error)
