d8197f8e4b90c49c54a411f1bb4b33f76a732e04
danaelhe <42972711+danaelhe@users.noreply.github.com>
2024-01-05 13:06:35 -0500

    remove test script

--- testscript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DROPLET_ID=$(go run cmd/doctl/main.go compute droplet create --image ubuntu-20-04-x64 --size s-1vcpu-1gb --region nyc1 wb2 --ssh-keys 40563907 --wait -o json | jq '.[] | .id')
-go run cmd/doctl/main.go compute ssh $DROPLET_ID --ssh-retry-max 30  
-doctl compute droplet delete $DROPLET_ID
