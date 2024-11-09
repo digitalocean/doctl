7f6f8817c5a70906ae3036c6291836627361f892
Blesswin Samuel <bsamuel@digitalocean.com>
2024-11-10 01:08:12 +0530

    update Listener to accept inputs

--- pkg/listen/listen.go
+++ pkg/listen/listen.go
@@ -2,12 +2,15 @@ package listen
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/url"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"golang.org/x/term"
 )
 
 // SchemaFunc takes a slice of bytes and returns an io.Reader (See Listener.SchemaFunc)
@@ -96,8 +99,100 @@ func (l *Listener) Start() error {
 		}
 	}()
 
+	prevWidth, prevHeight := 0, 0
+	resizeTerminal := func() error {
+		width, height, err := term.GetSize(0)
+		if err != nil {
+			return fmt.Errorf("error getting terminal size: %w", err)
+		}
+		if width == prevWidth && height == prevHeight {
+			return nil
+		}
+		prevWidth = width
+		prevHeight = height
+
+		data := struct {
+			Op     string `json:"op"`
+			Width  uint16 `json:"width"`
+			Height uint16 `json:"height"`
+		}{
+			Op:     "resize",
+			Width:  uint16(width),
+			Height: uint16(height),
+		}
+		if err := c.WriteJSON(data); err != nil {
+			return fmt.Errorf("error writing to websocket: %w", err)
+		}
+		return nil
+	}
+
+	var keepaliveTickerC <-chan time.Time
+	var resizeCheckerC <-chan time.Time
+	var stdinCh chan string
+
+	if l.In != nil {
+		keepaliveTicker := time.NewTicker(30 * time.Second)
+		resizeChecker := time.NewTicker(250 * time.Millisecond)
+		keepaliveTickerC = keepaliveTicker.C
+		resizeCheckerC = resizeChecker.C
+		stdinCh = make(chan string)
+		go func() {
+			// Set terminal to raw mode
+			oldState, err := term.MakeRaw(int(l.In.Fd()))
+			if err != nil {
+				fmt.Println("Error setting terminal to raw mode:", err)
+				return
+			}
+			defer term.Restore(int(l.In.Fd()), oldState) // Restore terminal on exit
+
+			for {
+				var b [1]byte
+				_, err := l.In.Read(b[:]) // Read one byte at a time
+				if err != nil {
+					fmt.Println("Error reading from stdin:", err)
+					break
+				}
+
+				stdinCh <- string(b[0])
+			}
+		}()
+
+		if err := resizeTerminal(); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	writeStdin := func(v string) error {
+		data := struct {
+			Op   string `json:"op"`
+			Data string `json:"data"`
+		}{
+			Op:   "stdin",
+			Data: v,
+		}
+		if err := c.WriteJSON(data); err != nil {
+			return fmt.Errorf("error writing to websocket: %w", err)
+		}
+		return nil
+	}
+
 	for {
 		select {
+		case b := <-stdinCh:
+			if err := writeStdin(string(b[0])); err != nil {
+				fmt.Println("Error writing to websocket:", err)
+			}
+		case <-resizeCheckerC:
+			if err := resizeTerminal(); err != nil {
+				fmt.Println(err)
+			}
+		case <-keepaliveTickerC:
+			if err := writeStdin(string("")); err != nil {
+				fmt.Println("Error writing to websocket:", err)
+			}
+			// if err := c.WriteMessage(websocket.PingMessage, nil); err != nil {
+			// 	return err
+			// }
 		case <-done:
 			return nil
 		case <-interrupt:
