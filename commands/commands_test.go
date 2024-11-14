9eec56b8916d500749fff85ab08cba8eadb72299
Ravish Ahmad <rahmadkhan@digitalocean.com>
2024-11-14 19:49:08 +0530

    docr: Add unit tests for new Registries commands

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -174,6 +174,7 @@ type tcMocks struct {
 	projects              *domocks.MockProjectsService
 	kubernetes            *domocks.MockKubernetesService
 	registry              *domocks.MockRegistryService
+	registries            *domocks.MockRegistriesService
 	sshRunner             *domocks.MockRunner
 	vpcs                  *domocks.MockVPCsService
 	oneClick              *domocks.MockOneClickService
@@ -220,6 +221,7 @@ func withTestClient(t *testing.T, tFn testFn) {
 		kubernetes:            domocks.NewMockKubernetesService(ctrl),
 		databases:             domocks.NewMockDatabasesService(ctrl),
 		registry:              domocks.NewMockRegistryService(ctrl),
+		registries:            domocks.NewMockRegistriesService(ctrl),
 		sshRunner:             domocks.NewMockRunner(ctrl),
 		vpcs:                  domocks.NewMockVPCsService(ctrl),
 		oneClick:              domocks.NewMockOneClickService(ctrl),
@@ -279,6 +281,7 @@ func withTestClient(t *testing.T, tFn testFn) {
 		Kubernetes:        func() do.KubernetesService { return tm.kubernetes },
 		Databases:         func() do.DatabasesService { return tm.databases },
 		Registry:          func() do.RegistryService { return tm.registry },
+		Registries:        func() do.RegistriesService { return tm.registries },
 		VPCs:              func() do.VPCsService { return tm.vpcs },
 		OneClicks:         func() do.OneClickService { return tm.oneClick },
 		Apps:              func() do.AppsService { return tm.apps },
