c1ff4461d8ec501bc18c6db194c1b06ee38763b6
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2025-01-28 17:31:28 -0500

    load-balancers: make network-stack flag consistent

--- args.go
+++ args.go
@@ -386,7 +386,7 @@ const (
 	// ArgLoadBalancerNetwork is the type of network the load balancer is accessible from.
 	ArgLoadBalancerNetwork = "network"
 	// ArgLoadBalancerNetworkStack is the network stack type the load balancer will be configured with (e.g IPv4, Dual Stack: IPv4 and IPv6).
-	ArgLoadBalancerNetworkStack = "network_stack"
+	ArgLoadBalancerNetworkStack = "network-stack"
 
 	// ArgFirewallName is a name of the firewall.
 	ArgFirewallName = "name"
