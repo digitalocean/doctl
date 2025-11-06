c497e1b0a6c2bb3db2354d944e2d4ae18400136c
anup-deka <adeka@digitalocean.com>
2025-11-06 15:49:12 +0530

    Update godo version (#1788)

--- vendor/github.com/digitalocean/godo/godo.go
+++ vendor/github.com/digitalocean/godo/godo.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.167.0"
+	libraryVersion = "1.168.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
