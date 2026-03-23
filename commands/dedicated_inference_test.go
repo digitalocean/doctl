9656b933c148e35afda312c6a36b243d5559afff
rarora <rarora@digitalocean.com>
2026-03-23 18:01:30 +0530

    Feature/list accelerators dedicated inference (#1821)
    * create-di
    
    * delete_di
    
    * LIST_ACCELORATORS
    
    * intergration-tests-added-list-accelerators

--- commands/dedicated_inference_test.go
+++ commands/dedicated_inference_test.go
@@ -72,6 +72,8 @@ func TestDedicatedInferenceCommand(t *testing.T) {
 	}
 	assert.True(t, subcommands["create"], "Expected create subcommand")
 	assert.True(t, subcommands["get"], "Expected get subcommand")
+	assert.True(t, subcommands["delete"], "Expected delete subcommand")
+	assert.True(t, subcommands["list-accelerators"], "Expected list-accelerators subcommand")
 }
 
 func TestRunDedicatedInferenceCreate(t *testing.T) {
@@ -165,6 +167,85 @@ func TestRunDedicatedInferenceGet(t *testing.T) {
 func TestRunDedicatedInferenceGet_MissingID(t *testing.T) {
 	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
 		err := RunDedicatedInferenceGet(config)
+		assert.Error(t, err)
+	})
+}
+
+func TestRunDedicatedInferenceDelete(t *testing.T) {
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		tm.dedicatedInferences.EXPECT().Delete("00000000-0000-4000-8000-000000000000").Return(nil)
+
+		config.Args = append(config.Args, "00000000-0000-4000-8000-000000000000")
+		config.Doit.Set(config.NS, doctl.ArgForce, true)
+
+		err := RunDedicatedInferenceDelete(config)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRunDedicatedInferenceDelete_MissingID(t *testing.T) {
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		err := RunDedicatedInferenceDelete(config)
+		assert.Error(t, err)
+	})
+}
+
+func TestRunDedicatedInferenceListAccelerators(t *testing.T) {
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		testAccelerators := do.DedicatedInferenceAcceleratorInfos{
+			{
+				DedicatedInferenceAcceleratorInfo: &godo.DedicatedInferenceAcceleratorInfo{
+					ID:     "accel-1",
+					Name:   "gpu-mi300x1-192gb",
+					Slug:   "gpu-mi300x1-192gb",
+					Status: "active",
+				},
+			},
+			{
+				DedicatedInferenceAcceleratorInfo: &godo.DedicatedInferenceAcceleratorInfo{
+					ID:     "accel-2",
+					Name:   "gpu-mi300x1-192gb",
+					Slug:   "gpu-mi300x1-192gb",
+					Status: "active",
+				},
+			},
+		}
+
+		tm.dedicatedInferences.EXPECT().ListAccelerators("00000000-0000-4000-8000-000000000000", "").Return(testAccelerators, nil)
+
+		config.Args = append(config.Args, "00000000-0000-4000-8000-000000000000")
+
+		err := RunDedicatedInferenceListAccelerators(config)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRunDedicatedInferenceListAccelerators_WithSlug(t *testing.T) {
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		testAccelerators := do.DedicatedInferenceAcceleratorInfos{
+			{
+				DedicatedInferenceAcceleratorInfo: &godo.DedicatedInferenceAcceleratorInfo{
+					ID:     "accel-1",
+					Name:   "mi300x1-ghfpsf",
+					Slug:   "gpu-mi300x1-192gb",
+					Status: "ACTIVE",
+				},
+			},
+		}
+
+		tm.dedicatedInferences.EXPECT().ListAccelerators("00000000-0000-4000-8000-000000000000", "gpu-mi300x1-192gb").Return(testAccelerators, nil)
+
+		config.Args = append(config.Args, "00000000-0000-4000-8000-000000000000")
+		config.Doit.Set(config.NS, doctl.ArgDedicatedInferenceAcceleratorSlug, "gpu-mi300x1-192gb")
+
+		err := RunDedicatedInferenceListAccelerators(config)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRunDedicatedInferenceListAccelerators_MissingID(t *testing.T) {
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		err := RunDedicatedInferenceListAccelerators(config)
 		assert.Error(t, err)
 	})
 }
