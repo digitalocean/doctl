1fcbe5f18d7a3c9904c237180eb69f3d68ad70bb
Anit Gandhi <agandhi@digitalocean.com>
2026-03-30 09:59:01 -0500

    deps: update godo to v1.181.0

--- vendor/github.com/digitalocean/godo/godo.go
+++ vendor/github.com/digitalocean/godo/godo.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.178.0"
+	libraryVersion = "1.181.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
