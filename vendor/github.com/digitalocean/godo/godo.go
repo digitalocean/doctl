57da16a3eef404e56dcced3b023cc3d0e39a7d2e
Ganga Singh <gangasingh1807@gmail.com>
2026-06-09 17:26:49 +0530

    updated godo

--- vendor/github.com/digitalocean/godo/godo.go
+++ vendor/github.com/digitalocean/godo/godo.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.194.1"
+	libraryVersion = "1.195.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
