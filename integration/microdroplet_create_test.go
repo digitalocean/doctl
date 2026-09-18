63b7f90b6ecba1a81709f105a6ed49e1a398d766
Shubham Raj Gupta <shubhamrajgupta@digitalocean.com>
2026-09-18 14:35:24 +0530

    microdroplet: restore integration fixture fixes after rebase

--- integration/microdroplet_create_test.go
+++ integration/microdroplet_create_test.go
@@ -96,7 +96,7 @@ var _ = suite("compute/microdroplet/create", func(t *testing.T, when spec.G, it 
 
 const (
 	microDropletCreateOutput = `
-ID                                      Name                  Region    State       Size                 Networking    Source                           Endpoint    Ports    Created At
+ID                                      Name                  Region    State       Size                  Networking    Source                          Endpoint    Ports    Created At
 b2a2f7a4-8d34-4c1c-9c66-3f2b7f8f38f2    sammy-microdroplet    nyc1      creating    2vCPU/4096MiB/80GB    public        docker.io/library/nginx:1.27                8080     2026-07-16T10:00:00Z
 `
 	microDropletCreateResponse = `
