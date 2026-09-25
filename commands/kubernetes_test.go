b41bf4d36b3a28ebd94bbf90ef828a9d45da1143
kgautam <kgautam@digitalocean.com>
2026-09-25 12:54:52 +0530

    added tests for missing flag and explicit false

--- commands/kubernetes_test.go
+++ commands/kubernetes_test.go
@@ -896,6 +896,83 @@ func TestKubernetesCreate(t *testing.T) {
 		err := testK8sCmdService().RunKubernetesClusterCreate("s-1vcpu-2gb", 3)(config)
 		assert.NoError(t, err)
 	})
+
+	// Test NFS CSI plugin omitted: when the flag is not set, nfs_csi_plugin is left unset
+	// so the API can apply its default.
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		clusterName := "nfs-omit-cluster"
+		r := godo.KubernetesClusterCreateRequest{
+			Name:        clusterName,
+			RegionSlug:  "sfo2",
+			VersionSlug: "1.13.0",
+			NodePools: []*godo.KubernetesNodePoolCreateRequest{
+				{
+					Name:  clusterName + "-default-pool",
+					Size:  "s-1vcpu-2gb",
+					Count: 3,
+				},
+			},
+			MaintenancePolicy: &godo.KubernetesMaintenancePolicy{
+				StartTime: "00:00",
+				Day:       godo.KubernetesMaintenanceDayAny,
+			},
+			AutoUpgrade:  false,
+			SurgeUpgrade: true,
+			NfsCsiPlugin: nil,
+		}
+		tm.kubernetes.EXPECT().Create(&r).Return(&testCluster, nil)
+
+		config.Args = append(config.Args, clusterName)
+		config.Doit.Set(config.NS, doctl.ArgRegionSlug, "sfo2")
+		config.Doit.Set(config.NS, doctl.ArgClusterVersionSlug, "1.13.0")
+		config.Doit.Set(config.NS, doctl.ArgSizeSlug, "s-1vcpu-2gb")
+		config.Doit.Set(config.NS, doctl.ArgNodePoolCount, 3)
+		config.Doit.Set(config.NS, doctl.ArgMaintenanceWindow, "any=00:00")
+		config.Doit.Set(config.NS, doctl.ArgSurgeUpgrade, true)
+		// Do NOT set ArgEnableNfsCsiPlugin - simulates omitting --enable-nfs-csi-plugin
+
+		err := testK8sCmdService().RunKubernetesClusterCreate("s-1vcpu-2gb", 3)(config)
+		assert.NoError(t, err)
+	})
+
+	// Test NFS CSI plugin explicit false: --enable-nfs-csi-plugin=false is forwarded.
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		clusterName := "nfs-false-cluster"
+		r := godo.KubernetesClusterCreateRequest{
+			Name:        clusterName,
+			RegionSlug:  "sfo2",
+			VersionSlug: "1.13.0",
+			NodePools: []*godo.KubernetesNodePoolCreateRequest{
+				{
+					Name:  clusterName + "-default-pool",
+					Size:  "s-1vcpu-2gb",
+					Count: 3,
+				},
+			},
+			MaintenancePolicy: &godo.KubernetesMaintenancePolicy{
+				StartTime: "00:00",
+				Day:       godo.KubernetesMaintenanceDayAny,
+			},
+			AutoUpgrade:  false,
+			SurgeUpgrade: true,
+			NfsCsiPlugin: &godo.KubernetesNfsCsiPlugin{
+				Enabled: boolPtr(false),
+			},
+		}
+		tm.kubernetes.EXPECT().Create(&r).Return(&testCluster, nil)
+
+		config.Args = append(config.Args, clusterName)
+		config.Doit.Set(config.NS, doctl.ArgRegionSlug, "sfo2")
+		config.Doit.Set(config.NS, doctl.ArgClusterVersionSlug, "1.13.0")
+		config.Doit.Set(config.NS, doctl.ArgSizeSlug, "s-1vcpu-2gb")
+		config.Doit.Set(config.NS, doctl.ArgNodePoolCount, 3)
+		config.Doit.Set(config.NS, doctl.ArgMaintenanceWindow, "any=00:00")
+		config.Doit.Set(config.NS, doctl.ArgSurgeUpgrade, true)
+		config.Doit.Set(config.NS, doctl.ArgEnableNfsCsiPlugin, false)
+
+		err := testK8sCmdService().RunKubernetesClusterCreate("s-1vcpu-2gb", 3)(config)
+		assert.NoError(t, err)
+	})
 }
 
 func TestKubernetesUpdate(t *testing.T) {
@@ -1071,6 +1148,58 @@ func TestKubernetesUpdate(t *testing.T) {
 		config.Doit.Set(config.NS, doctl.ArgKubernetesEnableSSO, true)
 		config.Doit.Set(config.NS, doctl.ArgKubernetesSSOIssuerURL, "https://issuer.example")
 		config.Doit.Set(config.NS, doctl.ArgKubernetesSSOClientID, "oidc-client-id")
+
+		err := testK8sCmdService().RunKubernetesClusterUpdate(config)
+		assert.NoError(t, err)
+	})
+
+	// Test NFS CSI plugin omitted: when the flag is not set, nfs_csi_plugin is left unset.
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		r := godo.KubernetesClusterUpdateRequest{
+			Name: testCluster.Name,
+			Tags: testCluster.Tags,
+			MaintenancePolicy: &godo.KubernetesMaintenancePolicy{
+				StartTime: "00:00",
+				Day:       godo.KubernetesMaintenanceDayAny,
+			},
+			AutoUpgrade:  boolPtr(false),
+			NfsCsiPlugin: nil,
+		}
+		tm.kubernetes.EXPECT().Update(testCluster.ID, &r).Return(&testCluster, nil)
+
+		config.Args = append(config.Args, testCluster.ID)
+		config.Doit.Set(config.NS, doctl.ArgClusterName, testCluster.Name)
+		config.Doit.Set(config.NS, doctl.ArgTag, testCluster.Tags)
+		config.Doit.Set(config.NS, doctl.ArgMaintenanceWindow, "any=00:00")
+		config.Doit.Set(config.NS, doctl.ArgAutoUpgrade, false)
+		// Do NOT set ArgEnableNfsCsiPlugin - simulates omitting --enable-nfs-csi-plugin
+
+		err := testK8sCmdService().RunKubernetesClusterUpdate(config)
+		assert.NoError(t, err)
+	})
+
+	// Test NFS CSI plugin explicit false: --enable-nfs-csi-plugin=false is forwarded.
+	withTestClient(t, func(config *CmdConfig, tm *tcMocks) {
+		r := godo.KubernetesClusterUpdateRequest{
+			Name: testCluster.Name,
+			Tags: testCluster.Tags,
+			MaintenancePolicy: &godo.KubernetesMaintenancePolicy{
+				StartTime: "00:00",
+				Day:       godo.KubernetesMaintenanceDayAny,
+			},
+			AutoUpgrade: boolPtr(false),
+			NfsCsiPlugin: &godo.KubernetesNfsCsiPlugin{
+				Enabled: boolPtr(false),
+			},
+		}
+		tm.kubernetes.EXPECT().Update(testCluster.ID, &r).Return(&testCluster, nil)
+
+		config.Args = append(config.Args, testCluster.ID)
+		config.Doit.Set(config.NS, doctl.ArgClusterName, testCluster.Name)
+		config.Doit.Set(config.NS, doctl.ArgTag, testCluster.Tags)
+		config.Doit.Set(config.NS, doctl.ArgMaintenanceWindow, "any=00:00")
+		config.Doit.Set(config.NS, doctl.ArgAutoUpgrade, false)
+		config.Doit.Set(config.NS, doctl.ArgEnableNfsCsiPlugin, false)
 
 		err := testK8sCmdService().RunKubernetesClusterUpdate(config)
 		assert.NoError(t, err)
